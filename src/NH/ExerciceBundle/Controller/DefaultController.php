8237cefef39c87b8ee607ed1ecf2b71b2ac3ed28
Lara30 <havardnadine@yahoo.fr>
2017-10-24 16:23:18 +0200

    mardi fin

--- src/NH/ExerciceBundle/Controller/DefaultController.php
+++ src/NH/ExerciceBundle/Controller/DefaultController.php
@@ -3,11 +3,12 @@
 namespace NH\ExerciceBundle\Controller;
 
 use NH\ExerciceBundle\Entity\Task;
+use NH\ExerciceBundle\Form\TaskEditType;
+use NH\ExerciceBundle\Form\TaskType;
 use NH\ExerciceBundle\NHExerciceBundle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -35,41 +36,37 @@ class DefaultController extends Controller
         //création de l'entité
         $task = new Task();
 //        $task->setTask('écrire');
-        $task->setDate(new \DateTime('today'));
+        //on crée le FormBuilder grâce au service form factory
+        $form = $this->createForm(TaskType::class, $task);
+
+//        $task->setDate(new \DateTime('today'));
 
 //on utilise la méthode javascript createformbuilder
-        $form = $this->createFormBuilder($task)
+//        $form = $this->createForm(TaskType::class, $task);
 
-            //méthodes add pour chaque champ
-            ->add('image', FileType::class, array('label' => "image"))
-            ->add('titre', TextType::class, array('label' => "titre"))
-            ->add('task', TextareaType::class, array('label' => "article"))
-            ->add('date', DateType::class, array('label' => "date"))
+        /* //méthodes add pour chaque champ
+         ->add('image', FileType::class, array('label' => "image"))
+         ->add('titre', TextType::class, array('label' => "titre"))
+         ->add('task', TextareaType::class, array('label' => "article"))
+         ->add('date', DateType::class, array('label' => "date"))
 
-            ->add('save', SubmitType::class, array('label' => "Creer un article"))
-            ->getForm();
+         ->add('save', SubmitType::class, array('label' => "Creer un article"))
+         ->getForm();*/
 
 //on doit interroger la requête et savoir si on est en post
         //auquel cas on récupère le formulaire
-        if ($request->isMethod('POST')) {
-            $form->handleRequest($request);
 
-//si le formulaire est soumis on fait appel à doctrine et à entitymangaer
-            if ($form->isSubmitted() && $form->isValid()) {
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            //on récupère l'entitymanager = em
+            $em = $this->getDoctrine()->getManager();
+            //on "persiste" l'entité
+            $em->persist($task);
+            //on balance dans la base de données
+            $em->flush();
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée');
 
-                //on récupère l'entitymanager = em
-                $em = $this->getDoctrine()->getManager();
-
-                //on "persiste" l'entité
-                $em->persist($task);
-
-                //on balance dans la base de données
-                $em->flush();
-
-                $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée');
-                return $this->redirectToRoute('nh_exercice_homepage', array(
-                    'id' => $task->getId()));
-            }
+            return $this->redirectToRoute('nh_exercice_homepage', array(
+                'id' => $task->getId()));
         }
         return $this->render('NHExerciceBundle:Default:add.html.twig', array(
             'form' => $form->createView(),
@@ -88,4 +85,25 @@ class DefaultController extends Controller
         return $this->redirectToRoute('nh_exercice_valid', array(
             'id' => $suppr->getId()));
     }
+
+    public function editAction($id, Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $task = $em->getRepository('NHExerciceBundle:Task')->find($id);
+        if (null === $task) {
+            throw new NotFoundHttpException("l'annonce d'id ".$id." n'existe pas.");
+        }
+        $form = $this->get('form.factory')->create(TaskEditType::class, $task);
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            //inutle de persister ici, doctrine connaît déjà
+            $em->flush();
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce modifiée');
+            return $this->redirectToRoute('nh_exercice_add', array('id' => $task->getId()));
+        }
+        return $this->render('NHExerciceBundle:Default:edit.html.twig', array(
+            'task' => $task,
+            'form' => $form->createView(),
+        ));
+    }
 }
+
+
---
+++ src/NH/ExerciceBundle/Form/TaskType.php
@@ -0,0 +1,39 @@
+<?php
+namespace NH\ExerciceBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use NH\ExerciceBundle\Entity\Task;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('image', FileType::class, array(
+            'label' => "null"))
+        ->add('titre', TextType::class, array(
+            'label' => "titre"))
+        ->add('task', TextareaType::class, array(
+            'label' => "article"))
+        ->add('date', DateType::class, array(
+            'label' => "date"))
+
+        ->add('save', SubmitType::class, array(
+            'label' => "Creer un article"))
+            ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults(array(
+                'data_class' => Task::class,
+        ));
+    }
+}
+
+
